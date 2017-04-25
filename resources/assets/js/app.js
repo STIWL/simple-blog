b217926aaa0fc6346b68d605eb27c3a862ec6f51
yazfield <hanifi.yazid@gmail.com>
2017-04-25 21:11:28 +0100

    Using Laravel mix

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -13,8 +13,9 @@ require('./bootstrap');
  * the application, or feel free to tweak this setup for your needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+/*Vue.component('example', require('./components/Example.vue'));
 
 const app = new Vue({
     el: 'body'
 });
+*/
